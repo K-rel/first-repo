0525dde6f45fc361bdadfbb89475e58c9be845a9
K-rol <K-rol@github.com>
2019-09-16 21:00:45 +0200

    second commit

--- gallowgame.py
+++ gallowgame.py
@@ -1,40 +1,51 @@
-password="gallow"
-word = input("podaj jedno słowo do odgadnięcia: ")
-space=''
-e=0
-for i in range(len(word)):
-    word=list(word)
-    word.insert(0+e,' ')
-    e=e+2
-    space=space+' _'
-space=list(space)
-word=''.join(word)
-letter=''
-guess=''
-n=1
-loser=False
-while guess!=word:
-    letter=input("Oto słowo do odgadnięcia:"+''.join(space)+" podaj literę: ")
-    if letter in list(word):
-        foo = ([pos for pos, char in enumerate(word) if char == letter]) #znajduje tablice indeksów liter w słowie word
-        y=0
-        for x in foo:
-            space[foo[y]] = letter
-            y=y+1
-            space = ''.join(space)
-            guess = space
-            space = list(space)
-    else:
-        password=list(password)
-        z=password[0:n]
-        print(''.join(z))
-        n=n+1
-        if n-1==len("gallow"):
-            print("przegrałeś")
-            loser=True
+while True:
+    from getpass import getpass
+
+    password = "gallow"
+    word = getpass("podaj jedno słowo do odgadnięcia: ")
+    word_second = word
+    space = ''
+    e = 0
+    for i in range(len(word)):
+        word = list(word)
+        word.insert(e, ' ')  # między literami tworzy przerwę
+        e = e + 2
+        space = space + ' _'
+    space = list(space)
+    word = ''.join(word)
+    letter = ''
+    guess = ''
+    n = 1
+    loser = False
+    while guess != word:
+        letter = input("Oto słowo do odgadnięcia:" + ''.join(space) + " podaj literę lub odgadnij całe słowo: ")
+        if letter == word_second:
             break
-if loser==False:
-    print("Wygrałeś! Słowo to: "+word)
+        elif letter in list(word):
+            foo = (
+            [pos for pos, char in enumerate(word) if char == letter])  # znajduje tablice indeksów liter w słowie word
+            y = 0
+            for x in foo:
+                space[foo[y]] = letter
+                y = y + 1
+                space = ''.join(space)
+                guess = space
+                space = list(space)
+        else:
+            password = list(password)
+            z = password[0:n]
+            print(''.join(z))
+            n = n + 1
+            if n - 1 == len("gallow"):
+                print("przegrałeś")
+                loser = True
+                break
+    if loser == False:
+        print("Wygrałeś! Słowo to: " + word)
+    input("Press enter to continue")
+
+   
+
 
 
 
